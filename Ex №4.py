4217645cf34a191c50686be1a4b1a55f74eb876f
Valentin Chernyaev <sergeich1997@yandex.ru>
2016-10-26 01:38:55 +0500

    Доделанные задания №№4,6

---
+++ Ex №4.py
@@ -0,0 +1,73 @@
+﻿import string
+
+def Translate_word(word):
+ str_add=""
+ punct=""
+ rus_word=""
+
+ if word.istitle():
+  # Слово было заглавным
+   was_title=True
+ else:
+   was_title=False
+ # Последний символ знак пунктуации
+ if word[len(word)-1] in string.punctuation:
+    str_add=word[len(word)-1:len(word)]
+    # str_add="."
+    punct=True #слово было со знаком
+ if punct==True:
+    word_trans=word[0:len(word)-1].lower() # режем
+ else: # резать не надо
+    word_trans=word.lower()
+
+ if word_trans in A:
+    if was_title:
+      rus_word=A[word_trans].title()+str_add
+    else:
+      rus_word=A[word_trans]+str_add
+ else:
+      rus_word=word
+ return rus_word
+
+
+stroka_rus_spisok=[]
+A=dict()
+# Считать словарь
+file_dict = open('c:\en-ru.txt', 'r', encoding="UTF-8")
+
+
+i=1
+#------------Ввод словаря--------------
+for stroka in file_dict.readlines():
+    stroka_spisok=stroka.split()
+    key=stroka_spisok[0] # ключ английское слово
+    A[key]=stroka_spisok[2] # значение перевод
+    # print(i, key, A[key])
+    i+=1
+file_dict.close() # считан весь файл словаря
+
+file_in = open('c:\input.txt', 'r')
+file_out= open('c:\output.txt', "w",encoding="UTF-8")
+
+print("---------------------------------------------")
+for stroka in file_in.readlines(): # перебрать все строки
+    stroka_rus_spisok=[] # очистить список для перевода строки
+    stroka_spisok=stroka.split()
+
+    for eng_word in stroka_spisok: # перебрать строку по словам
+       rus_word=Translate_word(eng_word) # перевести слово функцией
+       stroka_rus_spisok.append(rus_word) # добавить перевод слова
+       # в список строки
+
+    print(' '.join(stroka_rus_spisok)) # вывод на экран
+    print (' '.join(stroka_rus_spisok), file=file_out)  # через print в файл
+
+
+
+print("---------------------------------------------")
+
+file_in.close()
+file_out.close()
+
+
+
---
+++ Ex №6/Ex 6.py
@@ -0,0 +1,49 @@
+﻿import string
+A=dict()
+
+# Открыть файл на чтение в кодировке UTF-8
+file_in = open('en-ru1.txt', 'r', encoding="UTF-8")
+
+
+i=1
+print("-----Чтение из файла------")
+# Считать файл построчно
+for stroka in file_in.readlines():
+ stroka_spisok=[]
+# Отделить английское слово от перевода
+ stroka_spisok=stroka.split('\t-\t')
+
+#  убрать символы перевода строк
+ string=stroka_spisok[1]
+ if string[-1]=='\n':
+   stroka_spisok[1]=string[:-1]
+
+
+
+#------------ Формирование словаря---------------
+ key=stroka_spisok[1] # очередное русское слово
+ if key in A: # есть такое слово, добавляем новый перевод в список
+         A[key].append(stroka_spisok[0])
+ else: # создаём новый список перевода пока неизвестного русского слова
+         A[key] =[]
+         A[key].append(stroka_spisok[0])
+
+
+file_in.close() # считан весь исходный файл
+
+
+
+print("-----Сортировка------")
+# Словарь отсортировать нельзя, но можно сортировать ключи
+keys_sort = A.keys() # получаем ключи для сортировки
+keys_sort = list(keys_sort) # превращаем ключи в обычный список
+keys_sort.sort() # сортируем список ключей
+
+print("-----Вывод в файл------")
+# Открыть файл на запись в кодировке UTF-8
+file_out = open('ru_en.txt', 'w', encoding="UTF-8")
+for key in keys_sort: # вывод элементов словаря (ключ - значение) по алфавиту
+    string=str(key)+"-"+', '.join(A[key])
+    print(string, file=file_out)
+
+file_out.close()
